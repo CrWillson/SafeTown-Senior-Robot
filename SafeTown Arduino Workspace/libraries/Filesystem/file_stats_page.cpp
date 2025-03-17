e1564e5066fd439cef022b58e904c0063faa9954
CrWillson <calebwillson1@gmail.com>
2025-03-16 23:52:42 -0400

    Fixed file system stats page

--- SafeTown Arduino Workspace/libraries/Filesystem/file_stats_page.cpp
+++ SafeTown Arduino Workspace/libraries/Filesystem/file_stats_page.cpp
@@ -1,15 +1,15 @@
 #include "file_stats_page.hpp"
 #include <LittleFS.h>
-
+#include "menu.hpp"
 
 void FileStatPage::onPageLoad()
 {
     FSInfo fs_info;
     LittleFS.info(fs_info);
 
-    addLine("Total Bytes: " + std::to_string(fs_info.totalBytes));
-    addLine("Used Bytes: " + std::to_string(fs_info.usedBytes));
-    addLine("Available Bytes: " + std::to_string(fs_info.totalBytes - fs_info.usedBytes));
+    addLine(new TextMenuLine("Total Bytes: " + std::to_string(fs_info.totalBytes)));
+    addLine(new TextMenuLine("Used Bytes: " + std::to_string(fs_info.usedBytes)));
+    addLine(new TextMenuLine("Available Bytes: " + std::to_string(fs_info.totalBytes - fs_info.usedBytes)));
 
     addLine(new SpacerMenuLine());
     addLine(new ButtonMenuLine("Refresh", [this]{
